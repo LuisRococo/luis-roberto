93fc358a64e0233abc1631f7d3f5be78cffbfb5c
LuisRococo <luisrobertopenav@gmail.com>
2022-07-31 15:20:22 -0700

    Added Bash to stack

--- src/components/home/stack/data.ts
+++ src/components/home/stack/data.ts
@@ -1,7 +1,7 @@
 import { IconType } from "react-icons";
 import { BsGithub } from "react-icons/bs";
 import { FaDocker, FaReact } from "react-icons/fa";
-import { SiHtml5, SiJavascript, SiMongodb, SiNodedotjs, SiSass, SiTypescript, SiVisualstudio } from "react-icons/si";
+import { SiHtml5, SiJavascript, SiMongodb, SiNodedotjs, SiSass, SiTypescript, SiVisualstudio, SiGnubash } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
 import { AiFillGithub } from "react-icons/ai";
 
@@ -23,4 +23,5 @@ export const stackData: StackElementInterface[] = [
    { Icon: SiVisualstudio, name: "VS Code" },
    { Icon: AiFillGithub, name: "Github / Git" },
    { Icon: FaDocker, name: "Docker" },
+   { Icon: SiGnubash, name: "Bash" },
 ];
